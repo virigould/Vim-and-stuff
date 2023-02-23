11b0fc4b7d19be97e2f4a52df2a6d5fb56af2933
virigould <virigould@gmail.com>
2023-02-23 02:07:18 -0500

    setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "sorry but you gotta put the sudo password in to make sure you have all the dependencies. \nThis will also overwrite your vimrc but will put the old one in a backup in the same directory.";
+echo "sorry but you gotta put the sudo password in to make sure you have all the dependencies."; 
+exho "This will also overwrite your vimrc but will put the old one in a backup in the same directory.";
 sudo apt install wget curl vim;
-cp ~/.vimrc ~/./vimrc.old
-cp ./.vimrc ~/
+cp ~/.vimrc ~/.vimrc.old;
+cp ./.vimrc ~/;
 
