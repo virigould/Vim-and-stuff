20bcbad2aed24c4890e9e1c32823c08d61a0ab48
virigould <virigould@gmail.com>
2024-03-20 13:36:55 -0400

    adding in powerline configs and instructions

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.vimrc ~/.vimrc.old;
 cp ./.vimrc ~/.vimrc;
 cp ./.tmux.conf ~/.tmux.conf;
+echo "There are next steps now to get eveything in working order. \n First you must open tmux and do C-b + (shift + i) to install the plugins necessary for tmux.\n"
+echo "Then copy your theme and config files to the .config/tmux-powerline directory (themes goes in a /themes/ subdir and config.sh goes in the toplevel)"
+
