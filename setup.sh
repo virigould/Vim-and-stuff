62bd71290cc4b96363bcbb11f2974094dd355421
virigould <virigould@gmail.com>
2024-05-31 01:37:55 -0400

    added in the newvim config and a more robust setup script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,24 @@
 echo "sorry but you gotta put the sudo password in to make sure you have all the dependencies."; 
 echo "This will also overwrite your vimrc but will put the old one in a backup in the same directory.";
 # This will setup everything needed for both vim and tmux. 
-sudo apt install wget curl vim nmap net-tools git;
+sudo apt install wget curl vim nvim nmap net-tools git;
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.vimrc ~/.vimrc.old;
 cp ./.vimrc ~/.vimrc;
 cp ./.tmux.conf ~/.tmux.conf;
-echo "There are next steps now to get eveything in working order. \n First you must open tmux and do C-b + (shift + i) to install the plugins necessary for tmux.\n"
-echo "Then copy your theme and config files to the .config/tmux-powerline directory (themes goes in a /themes/ subdir and config.sh goes in the toplevel)"
+tmux new-session '~/.tmux/plugins/tpm/bin/install_plugins; sleep 5; exit'
+mkdir ~/.config/tmux-powerline/themes
+cp ./my-theme.sh ~/.config/tmux-powerline/themes/
+cp ./config.sh ~/.config/tmux-powerline/
 
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+
+echo "Alright so should just need to start nvim and decide if you want the default config or the one in the dir."
